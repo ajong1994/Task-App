953dfdfbbee20b68122eefc3a4710b1653224365
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-11-22 21:17:56 +0800

    Added Database Cleaner and modified spec variables accordingly

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  before :all do
+  before :each do
     Category.create(name:"Sample")
   end
   describe "GET /index" do
@@ -37,13 +37,13 @@ RSpec.describe "Categories", type: :request do
 
   describe '2. As a User, I want to edit a category to update the category\'s details' do
     it "GET /edit" do
-      get edit_category_path(43)
+      get edit_category_path(1)
       expect(response).to have_http_status(200)
       expect(response).to render_template(:edit)
     end
 
     it "PATCH /category" do
-      patch category_path(43), params: {category:{name:'Revised'}}
+      patch category_path(1), params: {category:{name:'Revised'}}
       # get categories_path
       expect(response).to redirect_to(categories_path)
       follow_redirect!
@@ -59,7 +59,7 @@ RSpec.describe "Categories", type: :request do
 
   describe '3. As a User, I want to view a category to show the category\'s details' do
     it "GET /:id" do
-      get category_path(43)
+      get category_path(1)
       expect(response).to have_http_status(200)
       expect(response).to render_template(:show)
     end
